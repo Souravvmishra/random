09b0eeae4f89ac297ff89e094362db5553c89c19
SouravvMishra <souravvmishra@gmail.com>
2023-05-07 17:20:40 +0530

    Fixed Moving Animation

--- src/App.js
+++ src/App.js
@@ -3,29 +3,37 @@ import ConfettiExplosion from 'react-confetti-explosion';
 
 const App = () => {
   const [isExploding, setIsExploding] = useState(false);
-  const [position, setPosition] = useState(0);
+  const [position, setPosition] = useState({ x: 30, y: 30 });
   const [showModal, setShowModal] = useState(false);
+  const [quote, setQuote] = useState("Awwww");
 
-  function getRandomInteger(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    // Generate a random number between 0 and (max - min)
-    var randomNumber = Math.floor(Math.random() * (max - min + 1));
-    // Add min to the random number to get a value between min and max
-    var randomInteger = randomNumber + min;
-    return randomInteger;
+  function getRandomPosition() {
+    // Get the viewport dimensions
+    var viewportWidth = window.innerWidth;
+    var viewportHeight = window.innerHeight;
+
+    // Set the maximum allowed position based on the viewport dimensions
+    var maxPositionX = viewportWidth * 0.1; // Adjust as needed
+    var maxPositionY = viewportHeight * 0.1; // Adjust as needed
+
+    // Generate random position values within the maximum allowed range
+    var positionX = Math.floor(Math.random() * maxPositionX);
+    var positionY = Math.floor(Math.random() * maxPositionY);
+
+    return { x: positionX, y: positionY };
   }
 
   const handleYes = () => {
-    setIsExploding(true);
+    setIsExploding(!isExploding);
+    getQuote()
     setTimeout(() => {
       setIsExploding(false);
       setShowModal(true);
-    }, 3000);
+    }, 1800);
   };
 
   const handleNo = () => {
-    setPosition(getRandomInteger(-100, 100));
+    setPosition(() => getRandomPosition())
   };
 
   const closeModal = () => {
@@ -34,12 +42,12 @@ const App = () => {
 
   const [password, setPassword] = React.useState("")
   useState(() => {
-    setPassword(() => prompt("ENTER PASSWORD", "f"))
+    setPassword(() => prompt("ENTER PASSWORD", "Bhai Rehne De Tu, Tere Liye Nahi Hai"))
     console.log(password);
-    
+
   }, [])
-  
-  if (password.toLowerCase() !== 'iloveyou') {
+
+  if (password.toLowerCase().trim() !== process.env.REACT_APP_PASSWORD) {
     return (
       <div className='flex justify-center items-center h-screen font-semibold text-3xl'>
         Loading ...
@@ -47,21 +55,40 @@ const App = () => {
     )
   }
 
+  const getQuote = async () => {
+    const url = 'https://quotes-by-api-ninjas.p.rapidapi.com/v1/quotes?category=love';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b4f68bc2c2mshe608a930ef4fc28p185c1djsn645ce3f43579',
+		'X-RapidAPI-Host': 'quotes-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.json();
+  setQuote(result[0].quote)
+} catch (error) {
+	console.error(error);
+}
+  }
+
 
   return (
     <>
       <div className="flex justify-center h-screen items-center bg-black font-mono">
-        <div className="flex flex-col items-center space-y-14">
-          <h1 className="text-5xl text-white font-bold underline m-14">Do You Love Sourav Mishra?</h1>
+        <div className="flex justify-center flex-col items-center  space-y-14">
+          <h1 className="text-5xl text-white font-bold underline mx-auto px-14">Do You Love Sourav Mishra?</h1>
 
-          <div className="flex space-x-6 text-2xl text-white m-4">
+          <div className="space-x-6 text-2xl text-white m-4">
             <button
               onClick={handleNo}
               style={{
-                left: `${position}%`,
-                top: `${position}%`,
+                left: `${position.x}%`,
+                bottom: `${position.y}%`,
               }}
-              className="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group"
+              className="absolute bg-black/50 inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group duration-300"
             >
               <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
               <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
@@ -75,7 +102,7 @@ const App = () => {
 
             <button
               onClick={handleYes}
-              className="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group"
+              className="absolute left-[50%] bottom-[30%] bg-black/50 inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group"
             >
               <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
               <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
@@ -85,8 +112,10 @@ const App = () => {
               <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
               <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
               <span className="relative">YES</span>
-              {isExploding && <ConfettiExplosion className="" particleCount={500} particleSize={15} duration={3000} force={0.8} width={1600} height={1600} />}
+              {isExploding && <ConfettiExplosion className="" particleCount={200} particleSize={15} duration={3000} force={0.8} width={1600} height={2200} />}
             </button>
+
+
           </div>
         </div>
       </div>
@@ -94,8 +123,8 @@ const App = () => {
         <div className="fixed z-10 inset-0 flex items-center justify-center">
           <div className="fixed inset-0 bg-black opacity-80"></div>
           <div className="relative bg-white rounded-lg p-8">
-            <h2 className="text-3xl text-gray-800  mb-4">I Love You Too ❤️😘</h2>
-            <p className="text-lg font-semibold text-gray-600 mb-4">Aww!</p>
+            <h2 className="text-3xl text-gray-800  mb-4">Sourav Mishra Loves You Too ❤️😘</h2>
+            <p className="text-lg font-semibold text-gray-600 mb-4">{quote}!</p>
             <button
               onClick={closeModal}
               className="bg-blue-500 text-white px-4 py-0.5 rounded-md"
